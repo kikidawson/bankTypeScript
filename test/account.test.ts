24c281a7fd61dd9a1a8bdafafcf9b2833fbfe68d
Kiki Dawson <kikiadawson@gmail.com>
2021-04-19 23:07:22 +0100

    Refactor account class

--- test/account.test.ts
+++ test/account.test.ts
@@ -4,7 +4,7 @@ import {expect} from 'chai'
 describe('bank account', () => {
     let account: Account
 
-    beforeEach(function() {
+    beforeEach(() => {
         account = new Account()
     })
 
